3fbcbde3695754f43d218a7653cdb9019b678afb
Torchz <fan.torchz@gmail.com>
2022-04-18 17:34:59 +0800

    hope this can create a new bad smell

--- src/main/java/com/weijilab/demo/DemoApplication.java
+++ src/main/java/com/weijilab/demo/DemoApplication.java
@@ -30,6 +30,7 @@ public class DemoApplication {
             result = result + String.format("| %-30s | %-42s |\n", stripAndTruncate(30, e.getKey()), stripAndTruncate(42, e.getValue()));
         }
 	if (false) {
+            result = String.format("| %-30s | %-42s |\n", stripAndTruncate(30, e.getKey()), stripAndTruncate(42, e.getValue()));
 	    return result;
 	}
         return result;
